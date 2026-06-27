f17e8aab954846a9e81b0a335359ce2cc589fc95
cubewhy <qby140326@gmail.com>
2026-06-27 10:40:01 +0800

    chore(rtc): remove old sync_time function

--- src/rtc.rs
+++ src/rtc.rs
@@ -19,32 +19,6 @@ pub fn init_timezone() {
     }
 }
 
-pub fn sync_time(rtc: &mut Ds1302) -> anyhow::Result<()> {
-    let naive_dt = rtc.read_time()?;
-
-    let timestamp_secs = if let Some(dt_utc) = chrono::Local.from_local_datetime(&naive_dt).single()
-    {
-        dt_utc.with_timezone(&Utc).timestamp()
-    } else {
-        naive_dt.and_utc().timestamp()
-    };
-
-    let tv = timeval {
-        tv_sec: timestamp_secs as _,
-        tv_usec: 0,
-    };
-
-    unsafe {
-        if settimeofday(&tv, std::ptr::null()) == 0 {
-            log::debug!("time sync completed: {tv:?}");
-        } else {
-            anyhow::bail!("failed to sync time to esp-idf");
-        }
-    }
-
-    Ok(())
-}
-
 pub fn sync_rtc_to_system(rtc: &mut Ds1302) -> anyhow::Result<()> {
     let naive_dt = rtc.read_time()?;
     let offset = FixedOffset::east_opt(TIMEZONE * 3600).unwrap();
